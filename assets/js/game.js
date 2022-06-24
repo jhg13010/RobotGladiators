1cddd9b2cbb4cce76014d10d95e2799b66a3d631
Jarrett Gray <jarrett.gray@uconn.edu>
2022-06-23 19:02:53 -0600

    added var playerName and fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
+//Initial js test 
 window.alert("This is an alert! JavaScript is running!");
+
+//Fetch user robot's name
+var playerName = window.prompt("What is your robot's name?");
+
+// this creates a function called "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+fight();
+
