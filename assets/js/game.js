3b6f1f395e31da2a6cc472185768b438e4d24501
Jarrett Gray <jarrett.gray@uconn.edu>
2022-06-25 18:12:12 -0600

    added js to update enemy health and player health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,40 @@
-//Initial js test 
-window.alert("This is an alert! JavaScript is running!");
-
-//Fetch user robot's name
+//fetch user robot's name
 var playerName = window.prompt("What is your robot's name?");
 
-// this creates a function called "fight"
-function fight () {
-    window.alert("The fight has begun!");
-}
-fight();
+//setup initial attributes
+var playerHealth = 100;
+var playerAttack = 10;
 
+//starting status
+console.log(playerName, playerHealth, playerAttack);
+
+//enemy Setup
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//enemy starting status
+console.log(enemyName, enemyHealth, enemyAttack);
+
+var fight = function() {
+    //alert players of start
+    window.alert("Welcome to Robot Gladiators!");
+
+    //subtract playerAttack from enemyHealth and update enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+    //console.log to confirm 
+    console.log(
+        playerName + " has attacked " + enemyName + ". " + enemyName + "'s heath updated to " + enemyHealth + "."
+        );
+
+    //subtract enemyAttack from playerHealth and update playerHealth
+    playerHealth = playerHealth - enemyAttack;
+    //console.log to confirm
+    console.log(
+        enemyName + "has attacked " + playerName + ". " + playerName + "'s health updated to " + playerHealth + "."
+        );
+
+
+}
+
+fight();
